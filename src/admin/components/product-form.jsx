903c4066b82a07bd0cd5e659946f918a88aedf67
NaveenkumarSimpleDev <128913146+NaveeenkumarSimpleDev@users.noreply.github.com>
2024-01-08 08:13:01 +0000

    Update 5 files

--- src/admin/components/product-form.jsx
+++ src/admin/components/product-form.jsx
@@ -1,6 +1,20 @@
-import { CATEGORIES } from "../../config";
+import { BRANDS, CATEGORIES } from "../../config";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectGroup,
+  SelectTrigger,
+  SelectValue,
+} from "../../components/ui/select";
+import { useState } from "react";
+import CreatableSelect from "react-select/creatable";
+import Button from "../../components/ui/button";
+import { PlusIcon } from "lucide-react";
 
 const ProductForm = () => {
+  const [isNewBrand, setNewBrand] = useState(false);
+  const [variationOpen, setVariationOpen] = useState(false);
   return (
     <div>
       <form action="">
@@ -38,6 +52,7 @@ const ProductForm = () => {
               className="px-2 py-1 border rounded-sm focus:outline-none border-[#eee]"
               name="price"
               id="price"
+              min={1}
             />
           </div>
 
@@ -50,15 +65,198 @@ const ProductForm = () => {
               className="px-2 py-1 border rounded-sm focus:outline-none border-[#eee]"
               name="discountPrice"
               id="discountPrice"
+              min={1}
             />
           </div>
         </div>
-        <div>
-            <select name="category" id="category">
+        <div className="w-[10rem] font-semibold space-y-2">
+          <label htmlFor="category" className=" font-bold text-xl">
+            Category
+          </label>
+          <Select>
+            <SelectTrigger>
+              <SelectValue
+                className="font-semibold"
+                placeholder="Select a category"
+              />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
                 {CATEGORIES.map((category) => (
-                  <option key={category} value={category}>{category}</option>
+                  <SelectItem
+                    key={category.id}
+                    className="font-semibold"
+                    value={category.value}
+                  >
+                    {category.label}
+                  </SelectItem>
                 ))}
-            </select>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+
+        {/* brand */}
+        <div className="space-y-2">
+          <label htmlFor="category" className=" font-bold text-xl">
+            Brand
+          </label>
+          <div className="font-semibold flex gap-4">
+            <div className="w-[12rem]">
+              <Select
+                onValueChange={(value) => {
+                  if (value === "new") {
+                    setNewBrand(true);
+                  } else {
+                    setNewBrand(false);
+                  }
+                }}
+              >
+                <SelectTrigger>
+                  <SelectValue
+                    className="font-semibold"
+                    placeholder="Select a Brand"
+                  />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    {BRANDS.map((brand) => (
+                      <SelectItem
+                        key={brand.id}
+                        className="font-semibold"
+                        value={brand.value}
+                      >
+                        {brand.label}
+                      </SelectItem>
+                    ))}
+                    <SelectItem className="font-semibold" value="new">
+                      Create new brand
+                    </SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+            {isNewBrand && (
+              <input
+                type="text"
+                className="px-2 py-1 border rounded-sm focus:outline-none border-[#eee]"
+                placeholder="Brand name"
+              />
+            )}
+          </div>
+        </div>
+
+        {/* Variations */}
+        <div>
+          <p className=" font-bold text-xl">Variations</p>
+          {!variationOpen && (
+            <Button type="button" onClick={() => setVariationOpen(true)}>
+              <PlusIcon className="h-4 w-4" />
+            </Button>
+          )}
+          {variationOpen && (
+            <>
+              <div className="space-y-4">
+                <p>Custom Attributes</p>
+                <div className="grid grid-cols-2 gap-4 w-full">
+                  <CreatableSelect
+                    isClearable
+                    options={[{ value: "test", label: "test" }]}
+                  />
+                  <input
+                    type="text"
+                    className="px-2 py-1 border rounded-sm focus:outline-none border-[#eee]"
+                    placeholder="Value"
+                  />
+                </div>
+                <div className="grid grid-cols-2 gap-4 w-full">
+                  <CreatableSelect
+                    isClearable
+                    options={[{ value: "test", label: "test" }]}
+                  />
+                  <input
+                    type="text"
+                    className="px-2 py-1 border rounded-sm focus:outline-none border-[#eee]"
+                    placeholder="Value"
+                  />
+                </div>
+                <div className="grid grid-cols-2 gap-4 w-full">
+                  <CreatableSelect
+                    isClearable
+                    options={[{ value: "test", label: "test" }]}
+                  />
+                  <input
+                    type="text"
+                    className="px-2 py-1 border rounded-sm focus:outline-none border-[#eee]"
+                    placeholder="Value"
+                  />
+                </div>
+                <p>
+                  Required : Please select at least one attribute for this
+                  product
+                </p>
+              </div>
+
+              <div className="flex gap-4">
+                <div className="flex flex-col ">
+                  <label htmlFor="var-price" className=" font-bold text-xl">
+                    Price
+                  </label>
+                  <input
+                    type="number"
+                    className="px-2 py-1 border rounded-sm focus:outline-none border-[#eee]"
+                    name="var-price"
+                    id="var-price"
+                    min={1}
+                  />
+                </div>
+                <div className="flex flex-col ">
+                  <label htmlFor="stock" className=" font-bold text-xl">
+                    Stock
+                  </label>
+                  <input
+                    type="number"
+                    className="px-2 py-1 border rounded-sm focus:outline-none border-[#eee]"
+                    name="stock"
+                    id="stock"
+                    min={1}
+                  />
+                </div>
+              </div>
+
+              <div className="flex flex-col ">
+                <label htmlFor="specs" className=" font-bold text-xl">
+                  Specifications
+                </label>
+                <input
+                  type="text"
+                  className="px-2 py-1 border rounded-sm focus:outline-none border-[#eee]"
+                  name="specs"
+                  id="specs"
+                />
+              </div>
+              <div className="flex flex-col ">
+                <label htmlFor="images" className=" font-bold text-xl">
+                  Images
+                </label>
+                <input
+                  type="file"
+                  className="px-2 py-1 border rounded-sm focus:outline-none border-[#eee]"
+                  name="images"
+                  id="images"
+                  multiple
+                />
+              </div>
+              <div>
+                <Button type="button" onClick={() => setVariationOpen(false)}>
+                  Save
+                </Button>
+                <Button type="button" onClick={() => setVariationOpen(false)}>
+                  Cancel
+                </Button>
+              </div>
+            </>
+          )}
         </div>
       </form>
     </div>
--- src/config/index.js
+++ src/config/index.js
@@ -1,35 +1,35 @@
 export const CATEGORIES = [
-    {
-        id: "1",
-        label: "Fashion",
-        value: 'Fashion',
-    },
-    {
-        id: "2",
-        label: "Electronics",
-        value: "Electronics"
-    },
-    {
-        id: "3",
-        label: "Grocery",
-        value: "grocery"
-    },
+  {
+    id: "1",
+    label: "Fashion",
+    value: "fashion",
+  },
+  {
+    id: "2",
+    label: "Electronics",
+    value: "electronics",
+  },
+  {
+    id: "3",
+    label: "Grocery",
+    value: "grocery",
+  },
 ];
 
 export const BRANDS = [
-    {
-        id: "b1",
-        label: "Apple",
-        value: "apple"
-    },
-    {
-        id: "b2",
-        label: "Nike",
-        value: "nike"
-    },
-    {
-        id: 'b3',
-        label: 'Puma',
-        value: "puma"
-    }
+  {
+    id: "b1",
+    label: "Apple",
+    value: "apple",
+  },
+  {
+    id: "b2",
+    label: "Nike",
+    value: "nike",
+  },
+  {
+    id: "b3",
+    label: "Puma",
+    value: "puma",
+  },
 ];
